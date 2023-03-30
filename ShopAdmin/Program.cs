1de7115f0c3957fa8799623ae24717a77355b22a
delia cirstea grenstadius <deliutacirstea@gmail.com>
2023-03-30 10:34:57 +0200

    Revert "Progress"

--- ShopAdmin/Program.cs
+++ ShopAdmin/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using ShopGeneral.Data;
 using ShopGeneral.Services;
 using Microsoft.EntityFrameworkCore;
-using ShopAdmin;
 
 var builder = ConsoleApp.CreateBuilder(args);
 builder.ConfigureServices((ctx, services) =>
@@ -41,24 +40,7 @@ using (var scope = app.Services.CreateScope())
 app.AddAllCommandType();
 app.Run();
 //generate prices to PriceRunner (JSON file)
-
-List<ProductModel> products = new()
-{
-    product.Add(new ProductModel(0, "test", );
-};
-
-//"id": 1,
-//      "title": "iPhone 9",
-//      "description": "An apple mobile which is nothing like apple",
-//      "price": 549,
-//      "discountPercentage": 12.96,
-//      "rating": 4.69,
-//      "stock": 94,
-//      "brand": "Apple",
-//      "category": "smartphones",
-//      "images": ["...", "...", "..."]
 //verify all product images exists 
-
 //report categories without products
 //report  
 
