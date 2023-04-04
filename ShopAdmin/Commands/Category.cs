6ecc11e0cb89c0d40710a8f66625c8f42285864e
NiclasEdstrom <113895402+NiclasEdstrom@users.noreply.github.com>
2023-04-04 10:12:04 +0200

    Progress

---
+++ ShopAdmin/Commands/Category.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using ShopAdmin.Models;
+using ShopGeneral.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAdmin.Commands
+{
+    public class Category
+    {
+        public class Product : ConsoleAppBase
+        {
+            private readonly ILogger<Product> _logger;
+            private readonly IProductService _productService;
+
+            public Product(ILogger<Product> logger, IProductService productService)
+            {
+                _logger = logger;
+                _productService = productService;
+            }
+
+
+            public void checkempty()
+            {
+
+                _logger.LogInformation("Checking category starting");
+
+                List<ProductModel> products = new();
+
+                foreach (var product in _productService.GetAllProducts())
+                {
+
+                    int category = product.CategoryId;
+
+
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+                string directoryPath = "outfiles\\categories\\";
+                string filePath = $"{directoryPath}\\missingcategories-{DateTime.UtcNow:yyyyMMdd}.txt";
+
+                _logger.LogInformation("Checking category ending");
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
